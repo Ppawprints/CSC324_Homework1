40f6da942c09febe598b5cb864ae622b287b95cd
Ppawprints <dongyila@grinnell.edu>
2020-04-01 18:31:03 -0400

    added model validations and resized images

--- hello_app/app/models/pic.rb
+++ hello_app/app/models/pic.rb
@@ -1,2 +1,6 @@
 class Pic < ApplicationRecord
+  validates(:animalType, presence: true)
+  validates(:picInfo, presence: true)
+  VALID_PIC_REGEX = /\A[\w+\-.]+\.+(jpeg|jpg|png|gif|bmp|webp|tiff)+\z/i
+  validates(:picInfo,  format: {with: VALID_PIC_REGEX})
 end
